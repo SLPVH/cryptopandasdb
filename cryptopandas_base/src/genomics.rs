98c5e2166187f8651b3b4fb30cef5b8d02b33906
Harry  Barber <hlbarber93@gmail.com>
2019-09-28 13:05:33 +0700

    Switch to reproducible random number generator

--- cryptopandas_base/src/genomics.rs
+++ cryptopandas_base/src/genomics.rs
@@ -1,29 +1,26 @@
-use rand::{Rng, SeedableRng, rngs::StdRng};
+use std::convert::TryInto;
+
+use oorandom::Rand64;
 use cashcontracts::single_sha256;
 
 /// Create a seed from a block hash and a transaction ID
-pub fn create_seed(block_hash: &[u8; 32], tx_id: &[u8; 32]) -> [u8; 32] {
-    single_sha256(&[&block_hash[..], &tx_id[..]].concat())
+pub fn create_seed(block_hash: &[u8; 32], tx_id: &[u8; 32]) -> u128 {
+    let digest = single_sha256(&[&block_hash[..], &tx_id[..]].concat());
+    u128::from_be_bytes(digest[..16].try_into().unwrap())
 }
 
 /// Mixes parents genes. Parents genes are given by 48 5-bit integers, 
 /// and represented by 48 bytes.
-pub fn mix_genes(mut m_genes: [u8; 48], mut s_genes: [u8; 48], seed: [u8; 32]) -> [u8; 48] {
-    /*
-    The algorithm is deterministic but should not be considered reproducible due to dependence on 
-    configuration and possible replacement in future library versions. 
-    For a secure reproducible generator, we recommend use of the rand_chacha crate directly.
-    */
-    // TODO: Make this reproducible using the rand_chacha crate
-    let mut rng: StdRng = SeedableRng::from_seed(seed);
+pub fn mix_genes(mut m_genes: [u8; 48], mut s_genes: [u8; 48], seed: u128) -> [u8; 48] {
+    let mut rng = Rand64::new(seed);
 
     // Scramble parent genes
     for i in (0..48).step_by(4) {
         for j in (1..4).rev() {
-            if rng.gen_range(0, 4) == 0 {
+            if rng.rand_range(0..4) == 0 {
                 m_genes.swap(i + j, i + j - 1);
             }
-            if rng.gen_range(0, 4) == 0 {
+            if rng.rand_range(0..4) == 0 {
                 s_genes.swap(i + j, i + j - 1);
             }
         }
@@ -48,7 +45,7 @@ pub fn mix_genes(mut m_genes: [u8; 48], mut s_genes: [u8; 48], seed: [u8; 32]) -
                 if gene_one > 23 {
                     sample_size *= 2;
                 }
-                if rng.gen_range(0, sample_size) == 0 {
+                if rng.rand_range(0..sample_size) == 0 {
                     mutation = gene_one / 2 + 16;
                 }
             }
@@ -58,7 +55,7 @@ pub fn mix_genes(mut m_genes: [u8; 48], mut s_genes: [u8; 48], seed: [u8; 32]) -
         if mutation != 0 {
             // Assign mutation to baby
             baby_genes[i] = mutation;
-        } else if rng.gen_range(0, 2) == 0 {
+        } else if rng.rand_range(0..2) == 0 {
             baby_genes[i] = m_genes[i];
         } else {
             baby_genes[i] = s_genes[i];
